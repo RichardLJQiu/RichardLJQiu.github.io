24d0dc612a8b7eb6c06dcb5717ba9811d5247594
RichardLJQiu <RichardLJQiu@users.noreply.github.com>
2018-10-05 23:40:02 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,57 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Denver, Colorado",
+    39.7392,
+    -104.9903
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Portland, Oregon",
+    45.5122,
+    -122.6587
+  ],  
+  [
+    "Washington, D.C.",
+    38.9072,
+    -77.0369
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Boston, Massachusetts",
+    42.3601,
+    -171.0589
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Baltimore, Maryland",
+    39.2904,
+    -76.6122
   ]
+  [
+    "St. Louis, Missouri",
+    38.6270,
+    -90.1994
+  ]  
+  [
+    "Miami, Florida",
+    25.7617,
+    -80.1918
+  ] 
+  [
+    "Dallas, Texas",
+    32.7767,
+    -96.7970
+  ]  
+  [
+    "Cleveland, Ohio",
+    41.4993,
+    -81.6944
+  ]   
+  [
+    "Cranberry Township, PA",
+    40.6850,
+    -80.1071
+  ]   
+  [
+    "Columbus, Ohio",
+    39.9612,
+    -82.9988
+  ]  
 ];
