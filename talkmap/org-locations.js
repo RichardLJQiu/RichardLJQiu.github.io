32ed1086761651548ba870c28c4586951377d1fb
RichardLJQiu <RichardLJQiu@users.noreply.github.com>
2018-10-06 00:18:42 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -2,7 +2,7 @@ var addressPoints = [
   ["Denver, Colorado",39.7392,-104.9903],
   ["Portland, Oregon",45.5122,-122.6587],
   ["Washington, D.C.",38.9072,-77.0369],
-  ["Boston, Massachusetts",42.3601,-171.0589],
+  ["Boston, Massachusetts",42.3601,-71.0589],
   ["Baltimore, Maryland",39.2904,-76.6122],
   ["St. Louis, Missouri",38.6270,-90.1994],
   ["Miami, Florida",25.7617,-80.1918],
